a694cec4c4478b905ddcb5f63151900be20f3685
baronhq <26222521@qq.com>
2018-05-10 17:30:53 +0800

    1

--- PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/Controllers/HomeController.cs
@@ -11,6 +11,9 @@ namespace PartyInvites.Controllers
         //GET: Home
         public ViewResult Index()
         {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+            ViewBag.hour = hour;
             return View();
         }
     }
