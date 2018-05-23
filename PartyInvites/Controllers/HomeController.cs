177ee0642dbc5cd13978b15fe40ddb5a48a75ccd
baronhq <26222521@qq.com>
2018-05-23 17:28:33 +0800

    1

--- PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/Controllers/HomeController.cs
@@ -15,6 +15,22 @@ namespace PartyInvites.Controllers
             return "Navigate to a URL to show an example";
         }
 
+        public ViewResult AutoProperty()
+        {
+            // create a new Product object
+            Product myProduct = new Product();
+
+            // set the property value
+            myProduct.Name = "Kayak";
+
+            // get the property
+            string productName = myProduct.Name;
+
+            // generate the view
+            return View("Result",
+                (object)String.Format("Product name: {0}", productName));
+        }
+
         [HttpGet]
         public ViewResult RsvpForm()
         {
