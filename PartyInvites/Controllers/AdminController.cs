8522bba9998e1e2db89ccf3a9bff4b6c8618cec9
baronhq <26222521@qq.com>
2018-05-23 16:43:03 +0800

    1

---
+++ PartyInvites/Controllers/AdminController.cs
@@ -0,0 +1,34 @@
+﻿using PartyInvites.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PartyInvites.Controllers
+{
+    public class AdminController : Controller
+    {
+        private IUserRepository repository;
+
+        public AdminController(IUserRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ActionResult ChangeLoginName(string oldName, string newName)
+        {
+            User user = repository.FetchByLoginName(oldName);
+            user.LoginName = newName;
+            repository.SubmitChanges();
+            // render some view to show the result
+            return View();
+        }
+
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
