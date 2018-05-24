11960af556b5c99a08d051a7236af4fe9a9473fd
baronhq <26222521@qq.com>
2018-05-24 16:20:32 +0800

    1

---
+++ PartyInvites/Models/ShoppingCart.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PartyInvites.Models
+{
+    public class ShoppingCart : IEnumerable<Product>
+    {
+        public List<Product> Products { get; set; }
+
+        public IEnumerator<Product> GetEnumerator()
+        {
+            return Products.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+    public class ShoppingCart1 : IEnumerable<Product>
+    {
+        public List<Product> Products { get; set; }
+
+        public List<Product> Products1 { get; set; }
+
+        public IEnumerator<Product> GetEnumerator()
+        {
+            return Products1.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
