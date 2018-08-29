4bffc7ac0475ffc854f386b428b7ed68465d5a28
baronhq <26222521@qq.com>
2018-08-29 14:59:46 +0800

    1

--- SportsStore.UnitTests/CartTests.cs
+++ SportsStore.UnitTests/CartTests.cs
@@ -1,13 +1,20 @@
 ﻿using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SportsStore.Domain.Entities;
+using Moq;
+using SportsStore.Domain.Abstract;
+using SportsStore.WebUI.Controllers;
+using System.Web.Mvc;
 
-namespace SportsStore.UnitTests {
+namespace SportsStore.UnitTests
+{
     [TestClass]
-    public class CartTests {
+    public class CartTests
+    {
 
         [TestMethod]
-        public void Can_Add_New_Lines() {
+        public void Can_Add_New_Lines()
+        {
 
             // Arrange - create some test products
             Product p1 = new Product { ProductID = 1, Name = "P1" };
@@ -28,7 +35,8 @@ namespace SportsStore.UnitTests {
         }
 
         [TestMethod]
-        public void Can_Add_Quantity_For_Existing_Lines() {
+        public void Can_Add_Quantity_For_Existing_Lines()
+        {
 
             // Arrange - create some test products
             Product p1 = new Product { ProductID = 1, Name = "P1" };
@@ -50,7 +58,8 @@ namespace SportsStore.UnitTests {
         }
 
         [TestMethod]
-        public void Can_Remove_Line() {
+        public void Can_Remove_Line()
+        {
 
             // Arrange - create some test products
             Product p1 = new Product { ProductID = 1, Name = "P1" };
@@ -74,7 +83,8 @@ namespace SportsStore.UnitTests {
         }
 
         [TestMethod]
-        public void Calculate_Cart_Total() {
+        public void Calculate_Cart_Total()
+        {
 
             // Arrange - create some test products
             Product p1 = new Product { ProductID = 1, Name = "P1", Price = 100M };
@@ -94,7 +104,8 @@ namespace SportsStore.UnitTests {
         }
 
         [TestMethod]
-        public void Can_Clear_Contents() {
+        public void Can_Clear_Contents()
+        {
 
             // Arrange - create some test products
             Product p1 = new Product { ProductID = 1, Name = "P1", Price = 100M };
@@ -114,5 +125,79 @@ namespace SportsStore.UnitTests {
             Assert.AreEqual(target.Lines.Count(), 0);
         }
 
+        [TestMethod]
+        public void Cannot_Checkout_Empty_Cart()
+        {
+
+            // Arrange - create a mock order processor
+            Mock<IOrderProcessor> mock = new Mock<IOrderProcessor>();
+            // Arrange - create an empty cart
+            Cart cart = new Cart();
+            // Arrange - create shipping details
+            ShippingDetails shippingDetails = new ShippingDetails();
+            // Arrange - create an instance of the controller
+            CartController target = new CartController(null, mock.Object);
+
+            // Act
+            ViewResult result = target.Checkout(cart, shippingDetails);
+
+            // Assert - check that the order hasn't been passed on to the processor
+            mock.Verify(m => m.ProcessOrder(It.IsAny<Cart>(), It.IsAny<ShippingDetails>()),
+                Times.Never());
+            // Assert - check that the method is returning the default view
+            Assert.AreEqual("", result.ViewName);
+            // Assert - check that I am passing an invalid model to the view
+            Assert.AreEqual(false, result.ViewData.ModelState.IsValid);
+        }
+
+        [TestMethod]
+        public void Cannot_Checkout_Invalid_ShippingDetails()
+        {
+
+            // Arrange - create a mock order processor
+            Mock<IOrderProcessor> mock = new Mock<IOrderProcessor>();
+            // Arrange - create a cart with an item
+            Cart cart = new Cart();
+            cart.AddItem(new Product(), 1);
+
+            // Arrange - create an instance of the controller
+            CartController target = new CartController(null, mock.Object);
+            // Arrange - add an error to the model
+            target.ModelState.AddModelError("error", "error");
+
+            // Act - try to checkout
+            ViewResult result = target.Checkout(cart, new ShippingDetails());
+
+            // Assert - check that the order hasn't been passed on to the processor
+            mock.Verify(m => m.ProcessOrder(It.IsAny<Cart>(), It.IsAny<ShippingDetails>()),
+                Times.Never());
+            // Assert - check that the method is returning the default view
+            Assert.AreEqual("", result.ViewName);
+            // Assert - check that I am passing an invalid model to the view
+            Assert.AreEqual(false, result.ViewData.ModelState.IsValid);
+        }
+
+        [TestMethod]
+        public void Can_Checkout_And_Submit_Order()
+        {
+            // Arrange - create a mock order processor
+            Mock<IOrderProcessor> mock = new Mock<IOrderProcessor>();
+            // Arrange - create a cart with an item
+            Cart cart = new Cart();
+            cart.AddItem(new Product(), 1);
+            // Arrange - create an instance of the controller
+            CartController target = new CartController(null, mock.Object);
+
+            // Act - try to checkout
+            ViewResult result = target.Checkout(cart, new ShippingDetails());
+
+            // Assert - check that the order has been passed on to the processor
+            mock.Verify(m => m.ProcessOrder(It.IsAny<Cart>(), It.IsAny<ShippingDetails>()),
+                Times.Once());
+            // Assert - check that the method is returning the Completed view
+            Assert.AreEqual("Completed", result.ViewName);
+            // Assert - check that I am passing a valid model to the view
+            Assert.AreEqual(true, result.ViewData.ModelState.IsValid);
+        }
     }
 }
--- SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
+++ SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -5,6 +5,7 @@ using SportsStore.Domain.Concrete;
 using SportsStore.Domain.Entities;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -34,15 +35,15 @@ namespace SportsStore.WebUI.Infrastructure
         private void AddBindings()
         {
             kernel.Bind<IProductRepository>().To<EFProductRepository>();
-            //Mock<IProductRepository> mock = new Mock<IProductRepository>();
-            //mock.Setup(m => m.Products).Returns(new List<Product>
-            //{
-            //    new Product { Name="Football",Price=25},
-            //    new Product { Name="Surf board",Price=179},
-            //    new Product { Name="Running shoes",Price=95}
-            //});
 
-            //kernel.Bind<IProductRepository>().ToConstant(mock.Object);
+            EmailSettings emailSettings = new EmailSettings
+            {
+                WriteAsFile = bool.Parse(ConfigurationManager
+                 .AppSettings["Email.WriteAsFile"] ?? "false")
+            };
+
+            kernel.Bind<IOrderProcessor>().To<EmailOrderProcessor>()
+                .WithConstructorArgument("settings", emailSettings);
         }
     }
 }
