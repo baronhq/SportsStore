71840e0a6af927dfd8dfd0d02ec8ca684cd31c35
baronhq <26222521@qq.com>
2018-10-26 11:28:39 +0800

    1

--- UrlsAndRoutes/App_Start/RouteConfig.cs
+++ UrlsAndRoutes/App_Start/RouteConfig.cs
@@ -19,6 +19,12 @@ namespace UrlsAndRoutes
 
             //routes.MapRoute("new1", "App/Do222{action}", new { controller = "Home", vara = "aa" });
 
+            routes.Add(new Route("SayHello", new CustomRouteHandler()));
+
+            routes.Add(new LegacyRoute(
+                  "~/articles/Windows_3.1_Overview.html",
+                  "~/old/.NET_1.0_Class_Library"));
+
             routes.MapRoute(name: "MyRoute", url: "{controller}/{action}");
 
             routes.MapRoute("MyOtherRoute", "App/{action}", new { controller = "Home" });
