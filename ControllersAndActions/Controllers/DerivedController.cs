ceb999c84d95fd9e0d4a70b2c655fbc6d71867bf
baronhq <26222521@qq.com>
2018-10-31 17:10:16 +0800

    1

---
+++ ControllersAndActions/Controllers/DerivedController.cs
@@ -0,0 +1,33 @@
+﻿using ControllersAndActions.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ControllersAndActions.Controllers
+{
+    public class DerivedController : Controller
+    {
+        // GET: Derived
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Hello from the DerivedController Indexmethod" + Request.UserHostAddress + "  " + Request.Url + "  " + RouteData.Route;
+            return View("MyView");
+        }
+
+        public ActionResult ProductOutput()
+        {
+            return Redirect("/Basic/Index");
+        }
+
+        public ActionResult RenameProduct()
+        {
+            string userName = User.Identity.Name;
+            string serverName = Server.MachineName;
+            string clientIP = Request.UserHostAddress;
+            DateTime dateStamp = HttpContext.Timestamp;
+            return View("MyView");
+        }
+    }
+}
---
+++ ControllersAndActions/Controllers/ExampleController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ControllersAndActions.Controllers
+{
+    public class ExampleController : Controller
+    {
+        // GET: Example
+        public ViewResult Index()
+        {
+            //ViewBag.Message =TempData["Message"];
+            //ViewBag.Date = TempData["Date"];
+
+            DateTime dt = DateTime.Now;
+            return View(dt);
+        }
+
+        //public RedirectResult Redirect()
+        //{
+        //    return RedirectPermanent("/Example/Index");
+        //}
+
+        public RedirectToRouteResult RedirectToRoute()
+        {
+            //return RedirectToRoute(new
+            //{
+            //    controller = "Example",
+            //    action = "Index",
+            //    ID = "MyID"
+            //});
+            TempData["Message"] = "hello1111";
+            TempData["Date"] = DateTime.Now;
+
+            DateTime dt = (DateTime)TempData.Peek("Date");
+            TempData.Keep("Date");
+            return RedirectToAction("index");
+        }
+
+        public HttpStatusCodeResult StatusCode()
+        {
+            return new HttpUnauthorizedResult();
+        }
+    }
+}
