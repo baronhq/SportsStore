db5b30f3088827a67e0f9255ba94b0a8345825fd
baronhq <26222521@qq.com>
2018-11-13 17:16:18 +0800

    1

---
+++ ControllerExtensibility/Controllers/CustomerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Mvc;
+using ControllerExtensibility.Models;
+
+namespace ControllerExtensibility.Controllers
+{
+    public class CustomerController : Controller
+    {
+
+        public ViewResult Index()
+        {
+            return View("Result", new Result
+            {
+                ControllerName = "Customer",
+                ActionName = "Index"
+            });
+        }
+
+        public ViewResult List()
+        {
+            return View("Result", new Result
+            {
+                ControllerName = "Customer",
+                ActionName = "List"
+            });
+        }
+    }
+}
