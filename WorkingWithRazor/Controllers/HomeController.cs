4956e08548a0480351c62fed65c6f7d4f3272b17
baronhq <26222521@qq.com>
2018-11-30 15:58:39 +0800

    1

--- WorkingWithRazor/Controllers/HomeController.cs
+++ WorkingWithRazor/Controllers/HomeController.cs
@@ -8,10 +8,21 @@ namespace WorkingWithRazor.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: Home
         public ActionResult Index()
         {
+            string[] names = { "Apple", "Orange", "Pear" };
+            return View(names);
+        }
+
+        public ActionResult List()
+        {
             return View();
+        }
+
+        [ChildActionOnly]
+        public ActionResult Time()
+        {
+            return PartialView(DateTime.Now);
         }
     }
 }
