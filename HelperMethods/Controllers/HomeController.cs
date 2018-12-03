5929f07130dcf3a8c2fe69a9313f5e45a5e25786
baronhq <26222521@qq.com>
2018-12-03 17:32:10 +0800

    1

--- HelperMethods/Controllers/HomeController.cs
+++ HelperMethods/Controllers/HomeController.cs
@@ -8,10 +8,15 @@ namespace HelperMethods.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: Home
         public ActionResult Index()
         {
-            return View();
+
+            ViewBag.Fruits = new string[] { "Apple", "Orange", "Pear" };
+            ViewBag.Cities = new string[] { "New York", "London", "Paris" };
+
+            string message = "This is an HTML element: <input>";
+
+            return View((object)message);
         }
     }
 }
