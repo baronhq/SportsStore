47d155cca71719aaef5e8f4d988a79b35b877282
baronhq <26222521@qq.com>
2018-12-05 17:20:46 +0800

    1

--- HelperMethods/Models/Metadata/PersonMetadata.cs
+++ HelperMethods/Models/Metadata/PersonMetadata.cs
@@ -24,5 +24,8 @@ namespace HelperMethods.Models
 
         [Display(Name = "Approved")]
         public bool IsApproved { get; set; }
+
+        [UIHint("Enum")]
+        public Role Role { get; set; }
     }
 }
