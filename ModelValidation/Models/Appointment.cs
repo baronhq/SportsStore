15a28191f905643b76f91ead30abe20b211ddf06
baronhq <26222521@qq.com>
2018-12-18 17:19:30 +0800

    1

--- ModelValidation/Models/Appointment.cs
+++ ModelValidation/Models/Appointment.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ModelValidation.Infrastructure;
+using System;
 using System.ComponentModel.DataAnnotations;
 using System.Web.Mvc;
 
@@ -6,7 +7,6 @@ namespace ModelValidation.Models
 {
     public class Appointment
     {
-
         [Required]
         [StringLength(10, MinimumLength = 3)]
         public string ClientName { get; set; }
@@ -15,6 +15,8 @@ namespace ModelValidation.Models
         [Remote("ValidateDate", "Home")]
         public DateTime Date { get; set; }
 
+        //[Range(typeof(bool), "true", "true", ErrorMessage = "You must accept ther terms.")]
+        [MustBeTrue(ErrorMessage = "You must accept ther terms1.")]
         public bool TermsAccepted { get; set; }
     }
 }
